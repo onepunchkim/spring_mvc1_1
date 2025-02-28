f9bcbd270f8e04f06d2b5da8d6cbb201392b07b4
onepunchkim <hyukjin4711@gmail.com>
2025-02-28 18:00:26 +0900

    spring request @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,12 +1,10 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -68,17 +66,16 @@ public class RequestParamController {
         return "ok";
     }
 
-    /**
-     * @RequestParam.required /request-param-required -> username이 없으므로 예외
-     * <p>
-     * 주의!
-     * /request-param-required?username= -> 빈문자로 통과
-     * <p>
-     * 주의!
-     * /request-param-required
-     * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는
-     * defaultValue 사용)
-     */
+    /*
+    * @RequestParam.required /request-param-required -> username이 없으므로 예외
+    * <p>
+    * 주의!
+    * /request-param-required?username= -> 빈문자로 통과
+    * <p>
+    * 주의!
+    * /request-param-required
+    * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는 defaultValue 사용)
+    * */
     @ResponseBody
     @RequestMapping("/request-param-required")
     public String requestParamRequired(
@@ -115,6 +112,29 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    /*
+     * @ModelAttribute 사용
+     * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model 을 설명할때 자세히 설명
+     * */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
+
+    /*
+     * @ModelAttribute 생략가능
+     * String, int 같은 단순 타입 = @RequestParam
+     * argument resolver 로 지정해둔 타입 외 = @ModelAttribute
+     * */
+    @ResponseBody
+    @RequestMapping("model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
 }
